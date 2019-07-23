8b71a9bf9802b1983db81ca383ebdfa43299b3e1
刘婷 <851491778@qq.com>
2019-07-23 21:28:40 +0800

    'update'

--- 常用功能代码.js
+++ 常用功能代码.js
@@ -84,3 +84,67 @@ const columns = [
 // ${params.consumerGroupId}
 
 // 删除保存: 数据放入body
+
+// 取局部样式
+// className={styles['last-half-row']}
+
+// 取全局样式
+//  className="classname"
+//  className={classnames('last-form-item', 'half-row')}
+
+//getFieldDecorator用法
+// 需要同时使用Form.create()
+const { getFieldDecorator } = this.props.form;
+{
+  getFieldDecorator(dataIndex, {
+    rules: [
+      {
+        required: true,
+        message: `Please Input ${title}!`
+      }
+    ],
+    initialValue: record[dataIndex]
+  })(this.getInput());
+}
+
+//提示框
+<Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel(record.key)}>
+  <a>Cancel</a>
+</Popconfirm>;
+
+//表单校验, 必输字段为空时返回undefined
+form.validateFields((error, row) => {
+  if (!error) {
+    // do something
+    return;
+  }
+});
+
+// 行内渲染
+const columns = {
+  title: '操作',
+  width: 100,
+  dataIndex: 'operation',
+  render: (text, record, index) => {
+    const isEdting = this.isEdting(index);
+    return isEdting ? (
+      <span>
+        <a onClick={() => this.onSave(index)} style={{ marginRight: 8 }}>
+        保存
+        </a>
+        <Popconfirm title="取消编辑?" onConfirm={() => this.onCancel(index)}>
+          <a>取消</a>
+        </Popconfirm>
+      </span>
+      ) : (
+      <span>
+        <a href="javascript:;" onClick={() => this.onEdit(index)} style={{ marginRight: 8 }}>
+        编辑
+        </a>
+        <a href="javascript:;">删除</a>
+      </span>
+    );
+  },
+},
+  
+  
